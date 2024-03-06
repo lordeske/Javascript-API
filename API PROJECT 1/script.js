e9f287287c17705a053138bae694a416f2fe5620
unknown <eskicmihajlo55@gmail.com>
2024-03-06 12:38:43 +0100

    created API read nad inster into page

--- API PROJECT 1/script.js
+++ API PROJECT 1/script.js
@@ -1,8 +1,32 @@
-const authorContainer  = document.getElementById("author-container");
-const loadMoreBtn  = document.getElementById("load-more-btn");
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+  .then((res) => res.json())
+  .then((data) => {
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  })
+  .catch((err) => {
+    console.error(`There was an error: ${err}`);
+  });
 
 
 
 
-fetch("https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-3c1bded3-0610-4289-a389-8d5d64326a93")
-.then((res)=> res.json()).then((data)=> console.log(data)).catch((err)=>console.error(`Desila se greska: ${err}`));
+  
+
+const displayAuthors = (authors) => {
+  authors.forEach(({ author, image, url, bio }, index) => {
+    authorContainer.innerHTML += `
+    <div id="${index}" class="user-card">
+      <h2 class="author-name">${author}</h2>
+
+    </div>
+  `;
+  });
+};
