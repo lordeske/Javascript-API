4cd5de133713957170dbeee955fe827bfe63ef33
unknown <eskicmihajlo55@gmail.com>
2024-03-08 13:22:38 +0100

    added category function

--- API PROJECT 2/script.js
+++ API PROJECT 2/script.js
@@ -7,12 +7,58 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
 
 
-const allCategories = {
+const allCategories = {  /// all categ object
 
     299 : {
         category : "Career Advice",
         className : "career",
+    },
+    409 : {
+        category : "Project Feedback",
+        className : "feedback",
+
+    },
+    417 : {
+        category : "freeCodeCamp Support",
+        className : "support",
+
+    },
+    421: { category: "JavaScript", className: "javascript" },
+    423: { category: "HTML - CSS", className: "html-css" },
+    424: { category: "Python", className: "python" },
+    432: { category: "You Can Do This!", className: "motivation" },
+    560: { category: "Backend Development", className: "backend" },
+}
+
+
+
+const forumCategory = (id) =>{
+
+    let selectedCategory  = {}
+
+    if (allCategories.hasOwnProperty(id))
+    {
+        const {category, className } = allCategories[id];
+        selectedCategory.className = className,
+        selectedCategory.category = category;
+
     }
+    else
+    {
+        selectedCategory.className = "general";
+        selectedCategory.category = "General";
+        selectedCategory.id = 1;
+
+    }
+
+
+    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+    const linkText = selectedCategory.category;
+
+    const linkClass = `category ${selectedCategory.className}`
+
+    return `<a href="${url}" class="${linkClass}" target="_blank">
+    ${linkText}</a>`;
 }
 
 
@@ -68,6 +114,7 @@ const fetchData = async () => {
         const res = await fetch(forumLatest);
         const data = await res.json();
         showLatestPosts(data);
+        console.log(data);
         
 
 
@@ -102,7 +149,8 @@ const showLatestPosts = (data) => {
         } = item;
 
         return `<tr>
-        <td><p class="post-title">${title}</p></td>   
+        <td><p class="post-title">${title}</p>
+            ${forumCategory(category_id)}</td>   
         <td></td>   
         <td>${posts_count - 1}</td>   
         <td>${viewCount(views)}</td>   
