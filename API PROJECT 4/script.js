e7c1a5fc20f800d59de12528df9aa8de30534347
Mihajlo Eskic <eskicmihajlo55@gmail.com>
2024-03-12 01:45:30 +0100

    changed fetch logic to catch errors, implemented change F to C

--- API PROJECT 4/script.js
+++ API PROJECT 4/script.js
@@ -1,5 +1,5 @@
-let currentCity = "Novi Sad";
-const units = "metric"
+let currentCity = "Belgrade";
+let units = "metric"
 
 
 /// selectors 
@@ -13,9 +13,43 @@ const weather__realfeel = document.querySelector('.weather__realfeel');
 const weather__humidity = document.querySelector('.weather__humidity');
 const weather__wind = document.querySelector('.weather__wind');
 const weather__pressure = document.querySelector('.weather__pressure');
+const weather__search = document.querySelector('.weather__search'); // form
+const weather__input = document.querySelector('.weather__searchform');
+const weather__cel = document.querySelector('.weather_unit_celsius');
+const weather__far = document.querySelector(`.weather_unit_farenheit`);
+
+weather__search.addEventListener("submit" , (e)=>{
+
+    e.preventDefault();
+
+    if(weather__input.value === "")
+    {
+        alert("Please enter Valid City Name");
+        return;
+
+    }
+
+
+    
+
+
+    currentCity = weather__input.value;
+    getWeather();
+    
+    weather__input.value="";
 
 
 
+
+
+
+
+})
+
+
+
+
+/// functions
 const convert = (country) => {  /// converting country code
 
     const regionNames = new Intl.DisplayNames(["en"],{type : "region"});
@@ -53,28 +87,94 @@ const convertTime = (time, zone) =>
 
 
 
-const getWeather = () => {
+
+
+
+
+
+/// API
+
+const getWeather = async () => {
 
     const apiKey = '64f60853740a1ee3ba20d0fb595c97d5';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`).then((resp)=> resp.json()).then(data => {
+    try 
+    {
 
+        const resp = await fetch(apiUrl);
+        
+        if(!resp.ok)
+        {
+            throw new Error(`HTTP error! Status: ${resp.status}`);
+
+        }
+
+
+        const data = await resp.json();
 
         console.log(data);
         city.innerHTML = `${data.name},${convert(data.sys.country)}`;
         datetime.innerHTML = convertTime(data.dt, data.timezone);
         weather__forecast.innerHTML = `<p>${data.weather[0].main}</p>`
-        weather__temperature.innerHTML =  `${Math.round(data.main.temp)}&#176C`;
+        weather__temperature.innerHTML =  `${Math.round(data.main.temp)}${units==="metric" ? "&#176C" : "F"}`;
         weather__icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png"/>`  // getting image from inpuut
-        weather__minmax.innerHTML = `<p>Min: ${Math.round(data.main.temp_min)}&#176C</p> <p>Max: ${Math.round(data.main.temp_max)}&#176C </p>`
-       
+        weather__minmax.innerHTML = `<p>Min: ${Math.round(data.main.temp_min)}${units==="metric" ? "&#176C" : "F"}</p> <p>Max: ${Math.round(data.main.temp_max)}${units==="metric" ? "&#176C" : "F"} </p>`
+        weather__realfeel.innerHTML = `${data.main.feels_like.toFixed(0)}${units==="metric" ? "&#176C" : "F"}`;
+        weather__humidity.innerHTML = `${data.main.humidity}%`;
+        weather__wind.innerHTML = `${data.wind.speed}${units==="imperial" ? "mph" : "m/s"} `;
+        weather__pressure.innerHTML = `${data.main.pressure} hPa`;
+        
 
     }
-    );
+    catch (err)
+    {
+
+        alert("Location not found");
+
+
+    }
+
+   
    
 
 }
 
 
-getWeather()
 
+
+weather__cel.addEventListener("click", ()=> {
+
+    if(units!=="metric")
+    {
+
+        units="metric";
+
+    }
+
+
+    getWeather();
+
+
+
+
+
+})
+
+
+
+weather__far.addEventListener('click', () => {
+    if(units !== "imperial"){
+      
+        units = "imperial"
+    
+        getWeather()
+    }
+})
+
+
+window.onload = function () {
+
+    getWeather();
+
+}
